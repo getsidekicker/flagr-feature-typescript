f0b205fb82bd4f630fee51471812bc8842d5ebd6
Tristan Kenney <tristan@kenney.co>
2022-02-01 18:13:44 +1100

    Update evaluator-stickiness.test.ts

--- src/evaluator-stickiness.test.ts
+++ src/evaluator-stickiness.test.ts
@@ -10,7 +10,7 @@ it('should maintain id stickiness', async () => {
   const evaluator = testCreateEvaluator();
   const id = randomString();
   const tags: NonEmptyArray<string> = [randomString()];
-  // 50% 'on'/ 50% off distribution
+  // 50% 'on'/50% off distribution
   const { flag } = await createFlag(tags, 50);
   await sleep(3000);
 
@@ -32,8 +32,8 @@ it('should maintain id stickiness', async () => {
     (set) => new Set(set.map((r) => JSON.stringify(r.results.get(flag.key))))
   );
 
-  // Set with consistent entityId - should either be entirely 'on' or 'off'
+  // Set with consistent entityId, should either be entirely 'on' or 'off'
   expect(evaluationSets[0].size).toBe(1);
-  // Set with random entityId – should evenly distribute between 'on' and 'off'
+  // Set with random entityId, should evenly distribute between 'on' and 'off'
   expect(evaluationSets[1].size).toBe(2);
 });
